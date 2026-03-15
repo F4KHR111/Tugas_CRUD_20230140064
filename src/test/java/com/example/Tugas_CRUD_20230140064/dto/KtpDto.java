b4cdd58048e8704b156abdb5f0202d6a38028bb1
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-03-15 14:54:49 +0700

    menambahkan field data diri pada KtpDto

--- src/test/java/com/example/Tugas_CRUD_20230140064/dto/KtpDto.java
+++ src/test/java/com/example/Tugas_CRUD_20230140064/dto/KtpDto.java
@@ -4,4 +4,10 @@ import lombok.Data;
 
 @Data
 public class KtpDto {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private Date tanggalLahir;
+    private String jenisKelamin;
 }
