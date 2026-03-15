8d29bf1e2d1d3de3f348420170d0f9be827d1289
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-03-15 14:11:38 +0700

    Setup Project

---
+++ src/main/java/com/example/Tugas_CRUD_20230140064/TugasCrud20230140064Application.java
@@ -0,0 +1,13 @@
+package com.example.Tugas_CRUD_20230140064;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrud20230140064Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrud20230140064Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tugas_CRUD_20230140064/TugasCrud20230140064ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tugas_CRUD_20230140064;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrud20230140064ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
