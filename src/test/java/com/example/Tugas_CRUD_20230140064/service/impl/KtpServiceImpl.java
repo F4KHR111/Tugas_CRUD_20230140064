34c2686b63c331c71f94dc97ef1c6647845901d4
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-03-15 15:31:04 +0700

    Menambahkan implementasi method getAll dan getById serta mapping entity ke DTO.

--- src/test/java/com/example/Tugas_CRUD_20230140064/service/impl/KtpServiceImpl.java
+++ src/test/java/com/example/Tugas_CRUD_20230140064/service/impl/KtpServiceImpl.java
@@ -26,7 +26,7 @@ public class KtpServiceImpl implements KtpService {
             throw new RuntimeException("Nomor KTP sudah ada");
         }
 
-        Ktp ktp = new Ktp();
+        ktp ktp = new ktp();
         ktp.setNomorKtp(dto.getNomorKtp());
         ktp.setNamaLengkap(dto.getNamaLengkap());
         ktp.setAlamat(dto.getAlamat());
@@ -39,5 +39,41 @@ public class KtpServiceImpl implements KtpService {
 
         return dto;
     }
+
+    @Override
+    public List<KtpDto> getAll() {
+
+        return repository.findAll()
+                .stream()
+                .map(k -> {
+                    KtpDto dto = new KtpDto();
+                    dto.setId(k.getId());
+                    dto.setNomorKtp(k.getNomorKtp());
+                    dto.setNamaLengkap(k.getNamaLengkap());
+                    dto.setAlamat(k.getAlamat());
+                    dto.setTanggalLahir(k.getTanggalLahir());
+                    dto.setJenisKelamin(k.getJenisKelamin());
+                    return dto;
+                }).collect(Collectors.toList());
+    }
+
+    @Override
+    public KtpDto getById(Integer id) {
+
+        ktp ktp = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Data tidak ditemukan"));
+
+        KtpDto dto = new KtpDto();
+
+        dto.setId(ktp.getId());
+        dto.setNomorKtp(ktp.getNomorKtp());
+        dto.setNamaLengkap(ktp.getNamaLengkap());
+        dto.setAlamat(ktp.getAlamat());
+        dto.setTanggalLahir(ktp.getTanggalLahir());
+        dto.setJenisKelamin(ktp.getJenisKelamin());
+
+        return dto;
+    }
+
 
 }
